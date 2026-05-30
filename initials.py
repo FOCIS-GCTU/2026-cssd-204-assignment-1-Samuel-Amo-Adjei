dc1130688ee0fc20cf99d0fd999a17f8e73a6281
Samuel-Amo-Adjei <2425400321@live.gctu.edu.gh>
2026-05-30 19:13:24 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: SAMUEL AMO ADJEI
+# STUDENT ID:  2425400321
+# Email: 2425400321@live.gctu.edu.gh
+# Grader:  Augustus
+#
+# On my honor, Samuel Amo Adjei, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
+    # Prints the initials SAA in small and large block letter format.
+    print()
+    print("...SAA")
+    print()
+    print("...SSSSSSSSSSSS............AAAA................AAAA.........")
+    print("...SS.....................AAAAAA..............AAAAAA........")
+    print("...SS....................AA....AA............AA....AA.......")
+    print("...SS...................AA......AA..........AA......AA......")
+    print("...SSSSSSSSSSSS.........AA......AA..........AA......AA......")
+    print(".............SS........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print(".............SS........AA........AA........AA........AA.....")
+    print(".............SS........AA........AA........AA........AA.....")
+    print(".............SS...**...AA........AA...**...AA........AA...**")
+    print("...SSSSSSSSSSSS...**...AA........AA...**...AA........AA...**")
+    print()
 
 main()
